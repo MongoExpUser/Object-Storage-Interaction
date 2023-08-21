5ca16b5f285f4846d05478a415cfd49320c2b958
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2023-08-20 23:36:48 -0500

    Update ObjectStorageInteraction.py

--- ObjectStorageInteraction.py
+++ ObjectStorageInteraction.py
@@ -99,7 +99,7 @@ class ObjectStorageInteraction():
             # print(object.key)
         # print(client.list_buckets())
         
-        # 3. sample_query() method within this class - see line 172 below.
+        # 3. sample_query() method within this class - see line 175 below.
         #    self.sample_query(file=None, input_serialization_option=None, client=None, bucket_name=None, file_name=None, sql_query_string=None, sample_one=True)
         
         # 4. note: for BIG DAT ANALYTICS:
